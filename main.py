b26323c7d1950cc541931f6bd73403ee95bc809e
Gustavo B <gustavobb@al.insper.edu.br>
2021-03-09 20:22:07 -0300

    v1.0

--- main.py
+++ main.py
@@ -1,74 +1,137 @@
 # macielcalebe
 import sys
 import re
+from enum import Enum
 
-operators = ['+', '-']
+types = ["INT", "PLUS", "SUB", "DIV", "MULT","EOF"]
 
-def raise_error(error: str) -> None:
-    raise ValueError(error)
-    exit(1)
+class Type(Enum):
+    INT = 1
+    PLUS = 2
+    SUB = 3
+    DIV = 4
+    MULT = 5
+    EOF = 6
 
-def extract_clean_input(input_argv: str) -> list:
-    ret_clean_input = []
-    tmp = list(filter(None, re.split('(\d+)', input_argv)))
-    for i in tmp: 
-        s = i.replace(" ", "")
-        if s != '':
-            ret_clean_input += [s]
+class Token:
 
-    print(ret_clean_input)
-    return ret_clean_input
+    def __init__(self, type_: Type, value: int):
+        self.type_ = type_
+        self.value = value
 
-def extract_operators_and_numbers_of_input(input_argv: list) -> list:
-    ret_operators = []
-    ret_numbers = []
-    found_number = False
-    found_operator = False
+class Tokenizer:
 
-    for token in input_argv:
+    def __init__(self, origin: str):
+        self.origin = origin
+        self.position = 0
+        self.actual = None
+        self.select_next()
 
-        if not token.isnumeric():
-            if token in operators and not found_operator:
-                ret_operators += [token]
-                found_operator = True
-                found_number = False
+    def select_next(self) -> Token:
+        token = None
+        if self.position == len(self.origin):
+            token = Token(Type.EOF, None)
+            self.actual = token
+            return
+
+        tmp = self.origin[self.position]
+    
+        if tmp == ' ': 
+            while (tmp == ' '):
+                self.position += 1
+                tmp = self.origin[self.position]
+
+        if tmp.isnumeric():
+            int_ = ''
+            while (True):
+                if len(self.origin) > self.position and self.origin[self.position].isnumeric():
+                    int_ += self.origin[self.position]
+                    self.position += 1
+                    continue
+
+                break
+        
+            self.position -= 1
+            token = Token(Type.INT, int(int_))
+
+        elif tmp == '+': 
+            token = Token(Type.PLUS, None)
+
+        elif tmp == '-': 
+            token = Token(Type.SUB, None)
+
+        elif tmp == '*': 
+            token = Token(Type.MULT, None)
+
+        elif tmp == '/': 
+            token = Token(Type.DIV, None)
+
+        else:
+            raise_error("not found operation")
+
+        self.actual = token
+        self.position += 1
+
+class Parser:
+
+    def __init__(self):
+        self.tokenizer = None
+        self.result = 0
+
+    def parse_expression(self) -> int:
+
+        if self.tokenizer.actual.type_ == Type.INT:
+            self.result = self.tokenizer.actual.value
+            self.tokenizer.select_next()
+
+            while (self.tokenizer.actual.type_ == Type.PLUS or self.tokenizer.actual.type_ == Type.SUB or 
+            self.tokenizer.actual.type_ == Type.DIV or self.tokenizer.actual.type_ == Type.MULT):
+                self.apply_operation(Type.PLUS, self.apply_sum)
+                self.apply_operation(Type.SUB, self.apply_sub)
+                self.apply_operation(Type.DIV, self.apply_division)
+                self.apply_operation(Type.MULT, self.apply_multiplication)
+
+                if self.tokenizer.actual.type_ != Type.EOF:
+                    self.tokenizer.select_next()
+                    
+                    if self.tokenizer.actual.type_ == Type.INT: raise_error("double int encountered")
+            
+            return self.result
+        
+        raise_error("do not start with operators")
+
+    def apply_operation(self, token_type: Type, func) -> int: 
+        if self.tokenizer.actual.type_ == token_type:
+            self.tokenizer.select_next()
+
+            if self.tokenizer.actual.type_ == Type.INT:
+                func()
 
             else:
-                raise_error("invalid operators")
-        
-        elif not found_number:
-            ret_numbers += [token]
-            found_number = True
-            found_operator = False
-        
-        else:
-            raise_error("double numbers")
+                raise_error("double operators encountered")
 
-    return ret_operators, ret_numbers
+    def apply_multiplication(self): 
+        self.result *= self.tokenizer.actual.value
 
-def invalid_operand_test(input_argv: list) -> None:
-    if not input_argv[0].isnumeric(): raise_error("first element must be int")
-    elif not input_argv[-1].isnumeric(): raise_error("last element must be int")
+    def apply_division(self): 
+        self.result /= self.tokenizer.actual.value
+
+    def apply_sub(self): 
+        self.result -= self.tokenizer.actual.value
+
+    def apply_sum(self): 
+        self.result += self.tokenizer.actual.value 
+
+    def code(self, code: str) -> int:
+        self.tokenizer = Tokenizer(code)
+        return self.parse_expression()
+
+def raise_error(error: str):
+    raise ValueError(error)
 
 def main(argv: str) -> int:
-    result = 0
-    
-    argv = extract_clean_input(argv)
-    invalid_operand_test(argv)
-
-    operators_in_argv, numbers_in_argv = extract_operators_and_numbers_of_input(argv)
-
-    final_list = [j for i in zip(numbers_in_argv, operators_in_argv) for j in i]
-    operation = 1
-
-    for element in final_list:
-        #print(element)
-        if element == '+': operation = 1
-        elif element == '-': operation = -1
-        else: result += int(element) * operation
-
-    result += int(numbers_in_argv[-1]) * operation
-    print(result)
+    parser = Parser()
+    print(parser.code(argv))
     return 0
 
 if __name__ == "__main__":
