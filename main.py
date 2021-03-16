e813ab9a39b8795150735161e55768f8ca6b2a19
Gustavo B <gustavobb@al.insper.edu.br>
2021-03-16 15:59:43 -0300

    v1.1

--- main.py
+++ main.py
@@ -39,6 +39,12 @@ class Tokenizer:
         if tmp == ' ': 
             while (tmp == ' '):
                 self.position += 1
+
+                if self.position == len(self.origin):
+                    token = Token(Type.EOF, None)
+                    self.actual = token
+                    return
+
                 tmp = self.origin[self.position]
 
         if tmp.isnumeric():
@@ -77,26 +83,16 @@ class Parser:
     def __init__(self):
         self.tokenizer = None
         self.result = 0
+        self.term_value = 0
 
     def parse_expression(self) -> int:
 
         if self.tokenizer.actual.type_ == Type.INT:
-            self.result = self.tokenizer.actual.value
-            self.tokenizer.select_next()
-
-            if self.tokenizer.actual.type_ == Type.INT: raise_error("double int encountered")
-            
-            while (self.tokenizer.actual.type_ == Type.PLUS or self.tokenizer.actual.type_ == Type.SUB or 
-            self.tokenizer.actual.type_ == Type.DIV or self.tokenizer.actual.type_ == Type.MULT):
+            self.term()
+            self.result = self.term_value
+            while (self.tokenizer.actual.type_ != Type.INT and self.tokenizer.actual.type_ != Type.EOF):
                 self.apply_operation(Type.PLUS, self.apply_sum)
                 self.apply_operation(Type.SUB, self.apply_sub)
-                self.apply_operation(Type.DIV, self.apply_division)
-                self.apply_operation(Type.MULT, self.apply_multiplication)
-
-                if self.tokenizer.actual.type_ != Type.EOF:
-                    self.tokenizer.select_next()
-                    
-                    if self.tokenizer.actual.type_ == Type.INT: raise_error("double int encountered")
             
             return self.result
         
@@ -105,35 +101,79 @@ class Parser:
     def apply_operation(self, token_type: Type, func) -> int: 
         if self.tokenizer.actual.type_ == token_type:
             self.tokenizer.select_next()
+            tmp = self.tokenizer.actual
 
-            if self.tokenizer.actual.type_ == Type.INT:
+            if tmp.type_ == Type.INT:
+                self.term()
                 func()
 
-            else:
-                raise_error("double operators encountered")
+            else: raise_error("double operators encountered")
+
+    def term(self):
+        if self.tokenizer.actual.type_ != Type.EOF:
+            self.term_value = self.tokenizer.actual.value
+            self.tokenizer.select_next()
+            if self.tokenizer.actual.type_ == Type.INT: raise_error("double int encountered")
+
+        while (self.tokenizer.actual.type_ == Type.MULT or self.tokenizer.actual.type_ == Type.DIV):
+            tmp = self.tokenizer.actual.type_
+            self.tokenizer.select_next()
+
+            if self.tokenizer.actual.type_ == Type.INT:
+                if tmp == Type.MULT: self.apply_multiplication() 
+                else: self.apply_division()
+                self.tokenizer.select_next()
+
+            else: raise_error("double operators encountered")
 
     def apply_multiplication(self): 
-        self.result *= self.tokenizer.actual.value
+        self.term_value *= self.tokenizer.actual.value
 
     def apply_division(self): 
-        self.result /= self.tokenizer.actual.value
+        self.term_value /= self.tokenizer.actual.value
 
     def apply_sub(self): 
-        self.result -= self.tokenizer.actual.value
+        self.result -= self.term_value
+        self.term_value = 0
 
     def apply_sum(self): 
-        self.result += self.tokenizer.actual.value 
+        self.result += self.term_value
+        self.term_value = 0
 
     def code(self, code: str) -> int:
         self.tokenizer = Tokenizer(code)
         return self.parse_expression()
+
+class PrePro:
+
+    def filter(self, code: str) -> str:
+        comment = False
+        final_code = ''
+        i = 0
+
+        while (i < len(code)):
+            if not comment and i != len(code) - 1 and code[i] == "/" and code[i + 1] == "*": 
+                i += 1
+                comment = True
+                
+            elif comment:
+                if i == len(code) - 1: raise_error("not closed comment")
+                elif code[i] == "*" and code[i + 1] == "/": 
+                    comment = False
+                    i += 1
+    
+            else: final_code += code[i]
+
+            i += 1
+
+        return final_code
 
 def raise_error(error: str):
     raise ValueError(error)
 
 def main(argv: str) -> int:
     parser = Parser()
-    print(parser.code(argv))
+    print(parser.code(PrePro().filter(argv)))
     return 0
 
 if __name__ == "__main__":
