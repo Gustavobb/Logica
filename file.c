bb5d5ffea0b3054bb2fb79f9e770c5304a243851
Gustavo B <gustavobb@al.insper.edu.br>
2021-05-18 12:12:57 -0300

    2.3.1

--- file.c
+++ file.c
@@ -1,7 +1,20 @@
 {
     bool a;
-    
+    int b;
+    string c;
+
     a = true;
-    a = !a;
+    b = 1;
+
+    while(a)
+    {
+        b = b + 1;
+        if (b > 10) a = false;
+    }
+
+    c = "test";
+
+    println(c);
+    println(b);
     println(a);
 }
